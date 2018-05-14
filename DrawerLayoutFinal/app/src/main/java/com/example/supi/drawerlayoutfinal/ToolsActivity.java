dff678a5f04e4356a5fd75ef1a828e46b61005eb
Supriyo Mondal <supriyo414@outlook.com>
2018-05-14 12:49:36 +0530

    First commit

---
+++ DrawerLayoutFinal/app/src/main/java/com/example/supi/drawerlayoutfinal/ToolsActivity.java
@@ -0,0 +1,13 @@
+package com.example.supi.drawerlayoutfinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToolsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tools);
+    }
+}
